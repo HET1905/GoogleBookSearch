461816e1cc858dbc4bcc5ec17d014cb890417875
Hetal Patel <nphetal1905@gmail.com>
2019-09-04 09:54:21 -0400

    view functionality added

--- client/src/components/Result/index.js
+++ client/src/components/Result/index.js
@@ -8,10 +8,28 @@ function Result(props) {
       <h3>Title : {props.title}</h3>
       <h4>Author : {props.author}</h4>
       <div className="btnGroup">
-        <button className="btn btn-primary"
-                onClick={()=>props.viewOnClick(props.title)}>View</button>
-        <button className="btn btn-primary" 
-                onClick={() => props.onSaveClick(props.title,props.author,props.imgLink,props.description,props.link)}>Save</button>
+        <a href={props.link}>
+          <button
+            className="btn btn-primary"
+           
+          >
+            View
+          </button>
+        </a>
+        <button
+          className="btn btn-primary"
+          onClick={() =>
+            props.onSaveClick(
+              props.title,
+              props.author,
+              props.imgLink,
+              props.description,
+              props.link
+            )
+          }
+        >
+          Save
+        </button>
       </div>
       <p className="clear"></p>
       <div className="row">
@@ -19,13 +37,12 @@ function Result(props) {
           <img src={props.imgLink} className="bookImg" alt={props.title}></img>
         </div>
         <div className="col-sm-10">
-        <p className="desc">{props.description}</p>
+          <p className="desc">{props.description}</p>
           <a href={props.link} className="clear">
             See the book
           </a>
         </div>
       </div>
-      
     </div>
   );
 }
